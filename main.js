ceddf2038a3420db29c9b5cb0022606f2f486dbe
Mark <markkranenburg@hotmail.nl>
2025-05-26 21:51:36 +0200

    add aria roles and label

--- main.js
+++ main.js
@@ -107,7 +107,7 @@ const main = (function () {
                 const row = board.getBoardArr()[i];
                 if (row[0].symbol === symbolToCheck && row[1].symbol === symbolToCheck && row[2].symbol === symbolToCheck) return true;
             }
-            
+
             // check is player has 3 on a row vertically
             for (let i = 0; i < 3; i++) {
                 if (cell[0][i].symbol === symbolToCheck && cell[1][i].symbol === symbolToCheck && cell[2][i].symbol === symbolToCheck) return true;
@@ -140,8 +140,8 @@ const main = (function () {
         const inputPlayerDisplayName = (e) => {
             const input = prompt('Enter name:');
             const name = input[0].toUpperCase() + input.slice(1);
-            if (e.target.id === 'playerOneDisplay') {setPlayerDisplayName(0, name)}
-                else {setPlayerDisplayName(1, name)};
+            if (e.target.id === 'playerOneDisplay') { setPlayerDisplayName(0, name) }
+            else { setPlayerDisplayName(1, name) };
             displayController.renderCurrentPlayerMarker();
         }
 
@@ -188,17 +188,20 @@ const main = (function () {
             displayBoard.innerHTML = ``;
 
             for (let i = 0; i < 3; i++) {
-                for(let j = 0; j < 3; j++) {
-                    displayBoard.innerHTML += `<div class='${boardArr[i][j].color === 'red' ? 'playerRed' : 'playerBlue'}' data-cellCoordinates="${i.toString() + j.toString()}">${boardArr[i][j].symbol}</div>`;
+                for (let j = 0; j < 3; j++) {
+                    displayBoard.innerHTML += `<div class='${boardArr[i][j].color === 'red' ? 'playerRed' : 'playerBlue'}' role="girdcell" tabindex="0" aria-label="Row ${i.toString()} Column ${j.toString()}" data-cellCoordinates="${i.toString() + j.toString()}">${boardArr[i][j].symbol}</div>`;
                 }
             }
+
+            console.log(displayBoard.innerHTML);
 
             const boardCells = displayBoard.querySelectorAll(':scope > div');
 
             boardCells.forEach(cell => {
                 cell.addEventListener('click', () => {
-                    if (board.getFreeze() === false)
+                    if (!board.getFreeze()) {
                         game.playRound(cell.dataset.cellcoordinates);
+                    }
                 });
             });
         }
@@ -210,10 +213,10 @@ const main = (function () {
             playerOneDisplay.innerHTML = players[0].displayName + ' <span class="playerSymbol playerBlue">X</span>';
             playerTwoDisplay.innerHTML = players[1].displayName + ' <span class="playerSymbol playerRed">O</span>';
 
-            if (currentPlayer === 'Player One' && !board.getFreeze()) {
+            if (currentPlayer === 'Player One') {
                 playerTwoDisplay.classList.remove('currentPlayer');
                 playerOneDisplay.classList.add('currentPlayer');
-            } else if (!board.getFreeze()) {
+            } else {
                 playerOneDisplay.classList.remove('currentPlayer');
                 playerTwoDisplay.classList.add('currentPlayer');
             }
@@ -251,16 +254,16 @@ const main = (function () {
         const toggleTheme = () => {
             if (document.documentElement.dataset.theme === 'dark') {
                 document.documentElement.removeAttribute('data-theme')
-                themeBtn.innerHTML = 
-                `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="43" height="43" fill="#000000">
+                themeBtn.innerHTML =
+                    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="43" height="43" fill="#000000">
                     <path
                         d="M6 2h8v2h-2v2h-2V4H6V2ZM4 6V4h2v2H4Zm0 10H2V6h2v10Zm2 2H4v-2h2v2Zm2 2H6v-2h2v2Zm10 0v2H8v-2h10Zm2-2v2h-2v-2h2Zm-2-4h2v4h2v-8h-2v2h-2v2Zm-6 0v2h6v-2h-6Zm-2-2h2v2h-2v-2Zm0 0V6H8v6h2Z">
                     </path>
                 </svg>`;
             } else {
                 document.documentElement.setAttribute('data-theme', 'dark')
-                themeBtn.innerHTML = 
-                `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="43" height="43" fill="#000000">
+                themeBtn.innerHTML =
+                    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="43" height="43" fill="#000000">
                     <path
                         d="M13 3h-2v2h2V3zm4 2h2v2h-2V5zm-6 6h2v2h-2v-2zm-8 0h2v2H3v-2zm18 0h-2v2h2v-2zM5 5h2v2H5V5zm14 14h-2v-2h2v2zm-8 2h2v-2h-2v2zm-4-2H5v-2h2v2zM9 7h6v2H9V7zm0 8H7V9h2v6zm0 0v2h6v-2h2V9h-2v6H9z">
                     </path>
